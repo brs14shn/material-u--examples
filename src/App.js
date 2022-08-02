8d3d40958910e122d7c38dc8714a762f429fb987
brs14shn <brsshn292929@gmail.com>
2022-08-02 15:07:00 +0300

    Header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-     <h1>App</h1>
+    <div>
+      <Header/>
+
+     
       
     </div>
   );
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import { AppBar } from "@mui/material";
+import React from "react";
+
+const Header = () => {
+  return;
+  <React.Fragment>
+    <AppBar></AppBar>
+  </React.Fragment>;
+};
+
+export default Header;
